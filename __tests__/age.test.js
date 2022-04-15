7ae7220239d935302f293290cc0c0c39e50b8ebf
Jake Edgar <jakeedgar1012@gmail.com>
2022-04-15 10:36:37 -0700

    revise previous tests to accomodate life expectency

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -5,21 +5,24 @@ describe('Age', () => {
   let reusableUser;
 
   beforeEach(() => {
-    reusableUser = new Age("Sam", 25);
+    reusableUser = new Age("Sam", 25, 75);
   });
 
   test('should return the name and age of the user based on input', () => {
     expect(reusableUser.name).toEqual("Sam");
     expect(reusableUser.age).toEqual(25);
+    expect(reusableUser.lifeExp).toEqual(75);
   });
 
   test("should return the age of user on mercury based on user age input", () => {
     reusableUser.planetAge("mercury");
     expect(reusableUser.age).toEqual(104);
+    expect(reusableUser.lifeExp).toEqual(312);
   });
 
   test("should return the age of user on venus based on user age input", () => {
     reusableUser.planetAge("venus");
     expect(reusableUser.age).toEqual(40);
+    expect(reusableUser.lifeExp).toEqual(122);
   });
 });
--- src/js/age.js
+++ src/js/age.js
@@ -1,7 +1,8 @@
 export default class Age {
-  constructor(name, age) {
+  constructor(name, age, lifeExp) {
     this.name = name;
     this.age = age;
+    this.lifeExp = lifeExp;
     this.planet = {};
   }
 
@@ -9,9 +10,11 @@ export default class Age {
   planetAge(planet) {
     if (planet === "mercury") {
       this.age = (Math.floor((this.age/.24)));
+      this.lifeExp = (Math.floor((this.lifeExp/.24)));
       this.planet = "mercury"
     } else if (planet === "venus") {
       this.age = (Math.floor((this.age/.61)));
+      this.lifeExp = (Math.floor((this.lifeExp/.61)));
       this.planet = "venus"
     } else {
       return this.age;
