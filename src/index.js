e7952f3602417d8e55c2b782bffeef3c9bafab91
Jake Edgar <jakeedgar1012@gmail.com>
2022-04-15 16:50:10 -0700

    final commit

--- src/index.js
+++ src/index.js
@@ -5,17 +5,16 @@ import Age from './js/age';
 $(document).ready(function() {
   $("#calc").submit(function(event) {
   event.preventDefault();
-  let user;
-  user = new Age(userName, ageInput, lifeInput)
-  console.log(user);
   const userName = $("#name-input").val();
   console.log(userName);
-  debugger;
   const ageInput = $("#age-input").val();
   console.log(ageInput);
   const lifeInput = $("#life-input").val();
   console.log(lifeInput);
-      const planet = $("input:radio[name=planet]:checked").val();
-      console.log(planet);
+  const planet = $("input:radio[name=planet]:checked").val();
+  console.log(planet);
+  let user;
+  user = new Age(userName, ageInput, lifeInput)
+  console.log(user);
   });
 });
